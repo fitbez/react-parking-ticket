a5d96e27dffbaaaabe0dea7aba2b86b73ba550ce
fitsum belayneh <fitsu_alt@yahoo.com>
2019-07-08 13:07:56 -0400

    card added for time stam component

--- src/TimeStamp/TimeStamp.js
+++ src/TimeStamp/TimeStamp.js
@@ -25,19 +25,55 @@ class TimeStamp extends Component {
 
   render() {
     return (
-      <div>
-        <h3>Clock In Time</h3>
-        <button onClick={this.ClockInEventHandler}>Clock In</button>
-        <button onClick={this.ClockOutEventHandler}>Clock Out</button>
+      <div
+        className="card"
+        style={{
+          border: "1px solid #eee",
+          width: "25%",
+          margin: "16px",
+          boxShadow:
+            "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
+          borderRadius: "5px"
+        }}
+      >
+        <div>
+          <h3>Clock In Time</h3>
+          <button
+            style={{
+              width: "120px",
+              height: "30px",
+              border: "1px solid #f4f4f4",
+              backgroundColor: "#009688",
+              margin: "8px",
+              color: "#ffffff"
+            }}
+            onClick={this.ClockInEventHandler}
+          >
+            Clock In
+          </button>
+          <button
+            style={{
+              width: "120px",
+              height: "30px",
+              border: "1px solid #f4f4f4",
+              backgroundColor: "#009688",
+              margin: "16px",
+              color: "#ffffff"
+            }}
+            onClick={this.ClockOutEventHandler}
+          >
+            Clock Out
+          </button>
 
-        <p>
-          {this.state.textIn}
-          {this.state.clockInTime}
-        </p>
-        <p>
-          {this.state.textOut}
-          {this.state.clockOutTime}
-        </p>
+          <p>
+            {this.state.textIn}
+            {this.state.clockInTime}
+          </p>
+          <p>
+            {this.state.textOut}
+            {this.state.clockOutTime}
+          </p>
+        </div>
       </div>
     );
   }
